7977dfdfdf2a29cc707fcc2b678e665b44a75c05
Ekaterina745 <112675236+Ekaterina745@users.noreply.github.com>
2022-09-02 12:03:55 +0900

    Add files via upload

---
+++ practich1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practich1
+{
+    public partial class Form1 : Form
+    {
+        public double x;
+        public double y;
+        public double z;
+        public double t;
+        public double PI = 3.1415926535897931;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            x = Convert.ToDouble(textBox1.Text);
+            y = Convert.ToDouble(textBox2.Text);
+            z= Convert.ToDouble(textBox3.Text);
+            t = (Math.Cos(x - PI / 6) * 2) / (0.5 + Math.Sin(y) * Math.Sin(y)) * (1+(z*z/((3-z*z)/5)));
+            textBox4.Text = Convert.ToString(t);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
